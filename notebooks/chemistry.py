27627dd4e7c970bad0e91fa1e33373969d7f936f
Gautham Ganapathy <gauthamg@gmail.com>
2019-06-26 00:30:02 +0100

    More bond length tuning

--- notebooks/chemistry.py
+++ notebooks/chemistry.py
@@ -4,8 +4,23 @@ import io
 import numpy as np
 from pprint import pprint
 
-cc1_err = 0.09
-err = 0.04
+import ase
+import ase.visualize
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+import plotly
+import plotly.graph_objs as go
+
+import ipyvolume.pylab as p3
+
+
+cc1_err = 0.19
+cn1_err = 0.32
+co1_err = 0.11
+no1_err = 0.1
+err = 0.05
 
 # bond_distances = {
 #     ('C', 'C'): (1.2, 1.54),
@@ -20,13 +35,36 @@ bond_distances = {
     ('C', 'C'): [(1.54 - cc1_err, 1.54 + cc1_err), (1.34 - err, 1.34 + err), (1.20 - err, 1.20 + err), (1.40 - err, 1.40 + err)],
     ('C', 'F'): [(1.34 - err, 1.34 + err)],
     ('C', 'H'): [(1.09 - err, 1.09 + err)],
-    ('C', 'N'): [(1.47 - err, 1.47 + err), (1.25 - err, 1.25 + err), (1.16 - err, 1.16 + err), (1.34 - err, 1.34 + err)],
-    ('C', 'O'): [(1.43 - err, 1.43 + err), (1.21 - err, 1.21 + err)],
-    ('N', 'H'): [(0.99 - err, 0.99 + err)],
-    ('O', 'H'): [(0.98 - err, 0.98 + err)],
+    ('C', 'N'): [(1.47 - cn1_err, 1.47 + cn1_err), (1.25 - err, 1.25 + err), (1.16 - err, 1.16 + err), (1.34 - err, 1.34 + err), (1.32 - err, 1.32 + err)],
+    ('C', 'O'): [(1.43 - co1_err, 1.43 + co1_err), (1.21 - err, 1.21 + err), (1.29 - err, 1.29 + err)],
+    ('F', 'N'): [(1.36 - err, 1.36 + err)],
+    ('H', 'N'): [(0.99 - err, 0.99 + err)],
+    ('H', 'O'): [(0.98 - err, 0.98 + err)],
+    ('N', 'N'): [(1.45 - err, 1.45 + err), (1.25 - err, 1.25 + err), (1.1 - err, 1.1 + err), (1.35 - err, 1.35 + err)],
+    ('N', 'O'): [(1.4 - no1_err, 1.4 + no1_err), (1.21 - err, 1.21 + err), (1.24 - err, 1.24 + err)]
+}
+
+atom_symbols = ['C', 'H', 'F', 'O', 'N']
+
+atom_color = {
+    'C': 'black',
+    'H': 'white',
+    'F': 'green',
+    'O': 'red',
+    'N': 'blue'
+}
+
+atom_size = {
+    'C': 12,
+    'H': 4,
+    'F': 19,
+    'O': 16,
+    'N': 14
 }
 
 class Atom:
+    __slots__ = ['symbol', 'position', 'index']
+
     def __init__(self, symbol, position, index=-1):
         self.symbol = symbol
         self.position = np.array(position)
@@ -37,6 +75,8 @@ class Atom:
     
 @dataclass
 class Bond:
+    __slots__ = ['atom1', 'atom2']
+
     atom1: Atom
     atom2: Atom
         
@@ -44,6 +84,8 @@ class Bond:
         return self.atom1.index == o.atom1.index and self.atom2.index == o.atom2.index
 
 class Molecule:
+    __slots__ = ['name', 'atoms', 'bonds']
+
     def __init__(self, name, atoms):
         self.name = name
         self.atoms = atoms
@@ -72,9 +114,13 @@ class Molecule:
         dist = self.__get_distance(atom1, atom2)
         pair = (atom1.symbol, atom2.symbol)
 
+        #s = set([8, 4])
         if pair in bond_distances:
             for bond_dist_min, bond_dist_max in bond_distances[pair]:
-                #print(pair, atom1.index, atom2.index, dist, bond_dist_min, bond_dist_max)
+                #print(pair, atom1.index, atom2.index, dist, bond_dist_min, bond_dist_max, dist >= bond_dist_min and dist <= bond_dist_max)
+                #if atom1.index in s and atom2.index in s:
+                #    print(pair, atom1.index, atom2.index, dist, bond_dist_min, bond_dist_max, dist >= bond_dist_min and dist <= bond_dist_max)
+
                 if dist >= bond_dist_min and dist <= bond_dist_max:
                     return Bond(atom1, atom2)
             
@@ -139,3 +185,79 @@ def compute_path(molecule, atomidx0, atomidx1):
         path.append(node)
 
     return list(reversed(path))
+
+def make_ase_atoms(molecule):
+    symbols = []
+    positions = []
+    data = []
+    for atom in molecule.atoms:
+        symbols.append(atom.symbol)
+        positions.append(atom.position)
+        data.append(atom.index)
+
+    return ase.Atoms(symbols=symbols, positions=positions), data
+
+def ase_plot_molecule(molecule):
+    aseatoms, data = make_ase_atoms(molecule)
+    return ase.visualize.view(aseatoms, viewer="x3d", data=data)
+
+def plt_plot_molecule(molecule):
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.gca(projection='3d')
+
+    x = [atom.position[0] for atom in molecule.atoms]
+    y = [atom.position[1] for atom in molecule.atoms]
+    z = [atom.position[2] for atom in molecule.atoms]
+    c = [atom_color[atom.symbol] for atom in molecule.atoms]
+    s = [20 * atom_size[atom.symbol] for atom in molecule.atoms]
+
+    ax.scatter(x, y, zs=z, c=c, s=s, edgecolor='black')
+
+    for atom in molecule.atoms:
+        ax.text(atom.position[0], atom.position[1], atom.position[2], '{} {}'.format(atom.symbol, atom.index))
+
+    for bond in molecule.bonds:
+        a1 = bond.atom1.position
+        a2 = bond.atom2.position
+        ax.plot([a1[0], a2[0]], [a1[1], a2[1]], [a1[2], a2[2]], 'black')
+
+    plt.show()
+
+def plotly_plot_molecule(molecule):
+    x = [atom.position[0] for atom in molecule.atoms]
+    y = [atom.position[1] for atom in molecule.atoms]
+    z = [atom.position[2] for atom in molecule.atoms]
+    c = [atom_color[atom.symbol] for atom in molecule.atoms]
+    s = [20 * atom_size[atom.symbol] for atom in molecule.atoms]
+
+    sg = go.Scatter3d(x=x, y=y, z=z,
+                      mode='markers',
+                      marker={
+                          'size': 10,
+                          'opacity': 1.0
+                      })
+    layout = go.Layout(margin={'l': 0, 'r': 0, 'b': 0, 't': 0})
+
+    figure = go.FigureWidget(data=[sg], layout=layout)
+    plotly.offline.iplot(figure)
+
+def ipv_plot_molecule(molecule):
+    for atom_symbol in atom_symbols:
+        atoms = [atom for atom in molecule.atoms if atom.symbol == atom_symbol]
+        if len(atoms) == 0:
+            continue
+
+        x = np.array([atom.position[0] for atom in atoms])
+        y = np.array([atom.position[1] for atom in atoms])
+        z = np.array([atom.position[2] for atom in atoms])
+
+        p3.scatter(x=x, y=y, z=z, color=atom_color[atom_symbol], size=atom_size[atom_symbol], marker='sphere')
+
+    for bond in molecule.bonds:
+        p1 = bond.atom1.position
+        p2 = bond.atom2.position
+        p3.plot(x=np.array([p1[0], p2[0]]),
+            y = np.array([p1[1], p2[1]]),
+            z = np.array([p1[2], p2[2]]),
+            color='black')
+    p3.show()        
